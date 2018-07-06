fb87802a9f067f57f3f19660a193d92a2e279df5
nurjongp <indonesia.nur@gmail.com>
2018-07-06 17:33:28 +0700

    pseudocode

---
+++ pseudocode/satu.js
@@ -0,0 +1,19 @@
+//Algoritma   : 1. masukkan m = massa benda (kg)
+//              2. masukkan a = percepatan benda (m/s2)
+//              3.ΣF = resultan gaya (Newton) adalah ΣF = m x a
+//              4. Hitung resultan gaya pada sebuah mobil
+
+//Pseudocode : STORE "m" with 600
+//           STORE "a" with 2
+//           STORE "ΣF" without any value
+
+//           calculate ΣF = m x a
+//           Print Resultan gaya ΣF
+
+
+var m = 600
+var a = 2
+var c = m*a
+
+console.log(c)
+ 
---
+++ pseudocode/tiga.js
@@ -0,0 +1,10 @@
+// algoritma   :1.Masukkan (jumlah Pakaian)
+//              2.Jika jumlah pakaian kurang dari 20 maka jumlah pakaian ditambah 1
+//              3.Jika jumlah pakaian = 20 maka tampilkan "Mesin Cuci Siap"
+
+// pseudocode  :1.Store "pakaian" 0 
+//              2.while ("pakaian" < 20) then
+ //                   "pakaian" = "pakaian" + 1
+//              then Display "Mesin Cuci Siap"
+
+
