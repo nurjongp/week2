ee0cf5b9e17dab2a67c67bade96d8f4f399c4cd3
nurjongp <indonesia.nur@gmail.com>
2018-07-11 19:26:32 +0700

    push2

---
+++ exe3_ifelse.js
@@ -0,0 +1,25 @@
+var nama = 'Danu';
+var peran = 'Penyihir';
+
+if (nama == '' && peran == ''){
+    console.log('Nama harus diisi')
+}
+else if(nama != '' && peran == ''){
+    console.log('Halo '  + nama + ' Pilih peranmu untuk memulai game!')
+}
+else if(nama == 'Nina' && peran == 'Ksatria' ) {
+    console.log('Selamat datang di Dunia Proxytia ,' + nama)
+    console.log('Halo ' + peran +' '+ nama + ' ,kamu dapat menyerang dengan senjatamu')
+}
+else if(nama == 'Danu' && peran == 'Tabib'){
+    console.log('Selamat datang di Dunia Proxytia ,' + nama )
+    console.log('Halo ' + peran +' ' + nama + ', kamu akan membantu temanmu yang terluka.')
+}
+else if(nama =='Zero' && peran == 'Penyihir'){
+    console.log('Selamat datang di Dunia Proxytia ,' + nama)
+    console.log('Halo ' + peran +' ' + nama +', ciptakan keajaiban yang membantu kemenanganmu!')
+}
+else {
+    console.log('Sorry bos ga ada')
+}
+
